edbf5ed5963f8915ff01d3db3f1d4e3c5c061d4a
soufiane-artisrt <mbank.globe@gmail.com>
2025-02-17 21:57:29 +0100

    new update

--- next.config.js
+++ next.config.js
@@ -1,48 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-  
-  // تكوين الإخراج لـ Vercel
-  distDir: '.next',
-  
-  // Disable telemetry and tracing
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  
-  // تكوين webpack مع تعطيل التتبع
-  webpack: (config, { isServer }) => {
-    // إضافة canvas كـ external
+  webpack: (config) => {
     config.externals = [...(config.externals || []), { canvas: 'canvas' }];
-    
-    // تعطيل التتبع
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: false,
-      };
-    }
-    
-    // تعطيل source maps
-    config.devtool = false;
-    
     return config;
-  },
-
-  // تكوين إضافي لـ Vercel
-  generateBuildId: () => 'build',
-  poweredByHeader: false,
-  generateEtags: false,
-  
-  // تكوين الصور
-  images: {
-    domains: ['localhost'],
-    unoptimized: true,
-  },
-
-  // Disable tracing
-  experimental: {
-    trace: false,
   },
 };
 
