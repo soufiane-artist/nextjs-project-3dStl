a21dbb70e0b4545fcbc070ce8fe56d5177fc9dd5
soufiane-artisrt <mbank.globe@gmail.com>
2025-03-05 04:55:37 +0000

    new update

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,7 @@ import ReduxProvider from './components/ReduxProvider'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
